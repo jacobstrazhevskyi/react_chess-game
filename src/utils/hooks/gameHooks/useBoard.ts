f9cf7d81a005ff3cf0e4d217221591f1975c8982
jacobstrazhevskyi <jacob.strazhevskyi@gmail.com>
2024-09-04 14:36:40 +0200

    implemented getter of possible selected figure moves

--- src/utils/hooks/gameHooks/useBoard.ts
+++ src/utils/hooks/gameHooks/useBoard.ts
@@ -1,11 +1,35 @@
+/* eslint-disable no-constant-condition */
 /* eslint-disable no-unused-vars */
-import { useState, useMemo, useContext } from 'react';
+import {
+  useState,
+  useMemo,
+  useContext,
+  useCallback,
+} from 'react';
+
 import { Board } from '../../../types/Board';
 import { Figure } from '../../../types/Figure';
 import { Cell } from '../../../classes/Cell';
 import { BoardContext } from '../useBoardContext';
 
+import { aux as settingsAux } from '../../../auх/settings';
+
+const {
+  BOARD_SIZE,
+} = settingsAux.settings;
+
+type Position = {
+  x: number,
+  y: number,
+};
+
+type GetFigureMovesProps = {
+  currentBoard: Board,
+  figure: Figure,
+};
+
 type SetFiguresProps = {
+  prevBoard: Board,
   whiteFigures: Figure[],
   blackFigures: Figure[],
 };
@@ -14,6 +38,7 @@ type ReturnedFromUseBoard = [
   Board,
   () => void,
   ({ whiteFigures, blackFigures }: SetFiguresProps) => void,
+  ({ currentBoard, figure }: GetFigureMovesProps) => Position[],
 ];
 
 export const useBoard = (): ReturnedFromUseBoard => {
@@ -24,24 +49,25 @@ export const useBoard = (): ReturnedFromUseBoard => {
   }
 
   const [board, setBoard] = useState<Board>(
-    new Array(8)
-      .fill(null).map((_, y) => new Array(8)
+    new Array(BOARD_SIZE)
+      .fill(null).map((_, y) => new Array(BOARD_SIZE)
         .fill(null).map((__, x) => new Cell({ x, y }, 0))),
   );
 
   const createBoard = () => {
     setBoard(
-      new Array(8)
-        .fill(null).map((_, y) => new Array(8)
+      new Array(BOARD_SIZE)
+        .fill(null).map((_, y) => new Array(BOARD_SIZE)
           .fill(null).map((__, x) => new Cell({ x, y }, 0))),
     );
   };
 
-  const setFigures = ({
+  const updateFigures = ({
+    prevBoard,
     whiteFigures,
     blackFigures,
   }: SetFiguresProps) => {
-    const newBoard = [...board];
+    const newBoard = [...prevBoard];
 
     whiteFigures.forEach((figure) => {
       const { x, y } = figure.position;
@@ -53,10 +79,72 @@ export const useBoard = (): ReturnedFromUseBoard => {
       newBoard[y][x].figure = figure;
     });
 
+    console.log(123);
+
     setBoard(newBoard);
   };
 
-  const value = useMemo(() => [board, createBoard, setFigures], [board]) as ReturnedFromUseBoard;
+  const isValidMove = useCallback(({ x, y }: Position, currentBoard: Board, figureColor: Figure['color']) => (
+    x >= 0 && x < BOARD_SIZE && y >= 0
+    && y < BOARD_SIZE
+    && (currentBoard[y][x].figure ? currentBoard[y][x].figure.color !== figureColor : true)
+  ), []);
+
+  const getFigureMoves = ({ currentBoard, figure }: GetFigureMovesProps): Position[] => {
+    const availableMoves: Position[] = [];
+
+    if (!figure) {
+      console.log(123);
+      return [];
+    }
+
+    const { x, y } = figure.position;
+
+    const { moves } = figure;
+
+    if (figure.figureType === 'bishop'
+      || figure.figureType === 'rook'
+      || figure.figureType === 'queen'
+    ) {
+      moves.forEach(([dx, dy]) => {
+        let newX = x;
+        let newY = y;
+
+        while (isValidMove({ x: newX, y: newY }, currentBoard, figure.color)) {
+          newX += dx;
+          newY += dy;
+
+          availableMoves.push({ x: newX, y: newY });
+        }
+      });
+
+      console.log(availableMoves);
+
+      return availableMoves;
+    }
+
+    moves.forEach(([dx, dy]) => {
+      const newX = x + dx;
+      const newY = y + dy;
+
+      if (isValidMove({ x: newX, y: newY }, currentBoard, figure.color)) {
+        availableMoves.push({ x: newX, y: newY });
+      }
+    });
+
+    console.log(moves);
+    console.log(figure);
+    console.log(availableMoves);
+
+    return availableMoves;
+  };
+
+  const value = useMemo(() => [
+    board,
+    createBoard,
+    updateFigures,
+    getFigureMoves,
+  ], [board]) as ReturnedFromUseBoard;
 
   return value;
 };
